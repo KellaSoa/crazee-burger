b0e1dbf8ceff5b65fe02be48cac19014fed5a9c0
Kella <fkellaie@gmail.com>
2023-08-12 17:48:17 +0200

    ✨ F03: LOGIN > style page with css

--- src/components/pages/login/LoginForm.jsx
+++ src/components/pages/login/LoginForm.jsx
@@ -1,5 +1,9 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import styled from "styled-components";
+import { theme } from "../../../theme";
+import { BsPersonCircle } from "react-icons/bs";
+import { IoChevronForward } from "react-icons/io5";
 
 export default function LoginForm() {
   const [userName, setuserName] = useState("");
@@ -15,10 +19,12 @@ export default function LoginForm() {
     setuserName(event.target.value);
   };
   return (
-    <div>
+    <LoginFormStyled action="action" onSubmit={handeSubmit}>
       <h1>bienvenue chez nous !</h1>
-      <h3>Connectez vous</h3>
-      <form action="action" onSubmit={handeSubmit}>
+      <hr />
+      <h2>Connectez vous</h2>
+      <div className="input-icon">
+        <BsPersonCircle className="icon" />
         <input
           type="text"
           value={userName}
@@ -26,8 +32,65 @@ export default function LoginForm() {
           required
           placeholder="Entrez votre prénom"
         />
-        <button>Accéder à mon espace</button>
-      </form>
-    </div>
+      </div>
+      <button className="btn-icon">
+        <span>Accéder à mon espace</span>
+        <IoChevronForward className="iconForward" />
+      </button>
+    </LoginFormStyled>
   );
 }
+
+const LoginFormStyled = styled.form`
+  letter-spacing: 1.5px;
+  font-family: "Amatic SC", cursive;
+
+  h1,
+  h2 {
+    color: ${theme.colors.white};
+    text-transform: capitalize;
+  }
+  hr {
+    border: 1.5px solid ${theme.colors.primary};
+    margin-bottom: 40px;
+  }
+  .input-icon {
+    background-color: white;
+    border-radius: 5px;
+    display: flex;
+    align-items: center;
+    padding: 18px 24px;
+    margin: 18px 0;
+
+    .icon {
+      font-size: 15px;
+      margin-right: 8px;
+      color: #93a2b1;
+    }
+
+    input {
+      border: none;
+      font-size: 15px;
+      width: 100%;
+      padding: 10x;
+    }
+    &::placeholder {
+      background: white;
+      color: lightgray;
+    }
+  }
+  .btn-icon {
+    font-family: initial;
+    color: white;
+    background-color: ${theme.colors.primary};
+
+    &:active {
+      color: white;
+      background-color: ${theme.colors.primary};
+      border: 1px solid ${theme.colors.primary};
+    }
+    width: 100%;
+    .iconForward {
+    }
+  }
+`;
--- src/components/pages/login/LoginPage.jsx
+++ src/components/pages/login/LoginPage.jsx
@@ -1,6 +1,27 @@
 import { useState } from "react";
 import LoginForm from "./LoginForm";
+import bg from "../../../assets/images/burgerbackground.jpg";
+import { styled } from "styled-components";
+import Logo from "../../reusable-ui/Logo";
 
 export default function LoginPage() {
-  return <LoginForm />;
+  return (
+    <LoginPageStyled className="container">
+      <Logo />
+      <LoginForm />
+    </LoginPageStyled>
+  );
 }
+
+const LoginPageStyled = styled.div`
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  background-image: url(${bg});
+  background-repeat: no-repeat;
+  background-blend-mode: darken;
+  background-size: cover;
+`;
