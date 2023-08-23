ffb257f9fd02b3de2ba6c13765131cef8d1211e1
kella <kella@a-piu.it>
2023-08-23 10:31:11 +0200

    💄 F06: NAVBAR >space button admin and profile

--- src/components/pages/order/Navbar/NavbarRigthSide.jsx
+++ src/components/pages/order/Navbar/NavbarRigthSide.jsx
@@ -1,10 +1,14 @@
 import { styled } from "styled-components";
 import Profile from "./Profile";
+import ToggleButton from "../../../reusable-ui/ToggleButton";
 
 export default function NavbarRigthSide() {
   return (
     <NavbarRigthSideStyled>
-      <div className="admin-btn"></div>
+      <ToggleButton 
+        labelIfChecked = "Désactiver le mode admin"
+        labelIfUnchecked = "Activer le mode admin"
+      />
       <Profile />
     </NavbarRigthSideStyled>
   );
