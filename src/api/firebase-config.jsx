cfe8423598897026ea8d7f628eb57eca25d5f0f0
kella <f.rianjafimamonjisoa@esis-italia.com>
2025-10-25 20:19:01 +0200

    🐛 F15: Fix config firebase

--- src/api/firebase-config.jsx
+++ src/api/firebase-config.jsx
@@ -1,24 +1,19 @@
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from "firebase/firestore";
 
-/*const firebaseConfig = {
-  apiKey: import.meta.env.VITE_APP_API_KEY,
-  authDomain: import.meta.env.VITE_APP_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_APP_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_APP_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_APP_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_APP_APP_ID,
-};*/
 const firebaseConfig = {
-  apiKey: "AIzaSyB2SdKfo0or09Z9W8ZMcD4nafbb6WZVpYI",
-  authDomain: "app-crazee-burger.firebaseapp.com",
-  projectId: "app-crazee-burger",
-  storageBucket: "app-crazee-burger.appspot.com",
-  messagingSenderId: "683526187850",
-  appId: "1:683526187850:web:14c05b89113eb169c86fb3",
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID,
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 const db = getFirestore(app);
-
 export { db };
